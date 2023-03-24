f3887067f7d32a2708d0d1ccd15921050ba3120f
OVYVO <vue12306@163.com>
2023-03-24 16:14:54 +0800

    🚀 tag: 第一阶段结束

--- lib/core/actions.ts
+++ lib/core/actions.ts
@@ -1,6 +1,7 @@
 import { cError } from '@utils/chalk'
 import { existsDir, removeDir } from '@utils/file'
 import { loadRemotePreset } from '@utils/loadRemotePreset'
+import inquirer from 'inquirer';
 
 interface Options{
   [index:string]: boolean
@@ -8,15 +9,30 @@ interface Options{
 
 // create指令
 export const handlerCreateProject = async(project:string, options:Options) => {
+  const { template } = await inquirer.prompt({
+    type: 'list',
+    name: 'template',
+    message: `请选择${project}项目需要的模板`,
+    choices:[{
+      name: '后台模板',
+      value: 1
+    },{
+      name: 'H5模板',
+      value: 2
+    },{
+      name: '普通模板',
+      value: 3
+    }]
+  })
   const isExistsDir = await existsDir(project)
   if(isExistsDir){
     if(options.force){
       await removeDir(project)
-      loadRemotePreset(project)
+      loadRemotePreset(project,template)
     }else{
       console.log(cError(`Folder '${project}' already exists!`))
     }
   }else{
-    loadRemotePreset(project)
+    loadRemotePreset(project,template)
   }
 }
--- lib/utils/loadRemotePreset.ts
+++ lib/utils/loadRemotePreset.ts
@@ -2,17 +2,21 @@ const { promisify } = require('util')
 const download = promisify(require('download-git-repo'))
 
 import { commandSpawn } from './terminal'
-import { vuePepo } from '../config/repo-config'
+import { vueRepo } from '../config/repo-config'
 import loading from './loading'
 import { cSuccess, cPrimary } from './chalk'
 
 const command = process.platform == 'win32' ? 'npm.cmd' : 'npm'
+const vueRepoMap = {
+  1: vueRepo
+}
 
-export const loadRemotePreset = async (project:string)=>{
+export const loadRemotePreset = async (project:string, type: number)=>{
   try {
+    if(type == 2 || type == 3) return console.log(cSuccess('模板正在准备中,请选择其他模板...'))
     console.log(cSuccess('Automatically creating project...'))
     loading.start({text:'File Creating...'})
-    await download(vuePepo, project, {clone: true})
+    await download(vueRepoMap[type], project, {clone: true})
     loading.succeed('File create done!')
     loading.start({text:'Dependent installing...'})
     await commandSpawn(command,['install'],{cwd: `./${project}`})
