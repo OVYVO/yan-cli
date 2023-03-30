059f2fd50903baf2cf7f98d3926a1a8c6ba18c42
OVYVO <vue12306@163.com>
2023-03-30 16:53:38 +0800

    :bug: fix: 命令行提示修复

--- lib/core/create.ts
+++ lib/core/create.ts
@@ -4,12 +4,12 @@ import { handlerCreateProject,handlerPrettifyProject } from './actions'
 export const createCommands = () => {
   program
     .command('create <project>')
-    .description('Create a new project from project template')
+    .description('Create a new project from project template. exp: yan create xxx')
     .option('--force', 'Force overwrite of target folder')
     .action(handlerCreateProject)
 
   program
     .command('prettify')
-    .description('Add code prettify tools, Eslint Prettier')
+    .description('Add code prettify tools, Eslint Prettier. exp: yan prettify')
     .action(handlerPrettifyProject)
 }
--- lib/core/help.ts
+++ lib/core/help.ts
@@ -4,7 +4,7 @@ const program = require('commander')
 export function helpOptions(){
   program.name(Pack.name)
   program.version(Pack.version)
-  program.option('-d, --dest <dest>','destination folder, exp: -d /src/pages')
-  program.option('-f, --framework <framework>','project framework, exp: vue')
+  // program.option('-d, --dest <dest>','destination folder, exp: -d /src/pages')
+  // program.option('-f, --framework <framework>','project framework, exp: vue')
   program.usage(`<command> [option]`)
 }
