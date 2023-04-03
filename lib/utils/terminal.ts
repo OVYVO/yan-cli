97b3ae9294bbe4a1cfd53e1efcb8fb99344e3ce3
OVYVO <vue12306@163.com>
2023-04-03 16:17:41 +0800

    :bug: fix: 修复terminal.ts错误

--- lib/utils/terminal.ts
+++ lib/utils/terminal.ts
@@ -5,14 +5,14 @@ export const commandSpawn = (...args: Parameters<typeof spawn>) =>{
   return new Promise<void>((resolve, reject) => {
     const childProcess:ChildProcess = spawn(...args)
     // 在父进程中显示子进程中的标准输出流输出的信息
-    // childProcess.stdout?.pipe(process.stdout)
+    childProcess.stdout?.pipe(process.stdout)
     // 在父进程中显示子进程中的标准输出流输出的错误信息
-    childProcess.stdout?.pipe(process.stderr)
+    childProcess.stderr?.pipe(process.stderr)
     childProcess.on('close',()=>{
       resolve()
     })
-    childProcess.on('error',()=>{
-      reject()
+    childProcess.on('error',(err)=>{
+      reject(err)
     })
   })
 }
