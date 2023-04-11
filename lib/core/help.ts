263838189c17db7cf0b86581cbc1d6e4fd8878df
OVYVO <vue12306@163.com>
2023-04-11 16:30:11 +0800

    :rocket: feat：版本1.0.4

--- lib/core/help.ts
+++ lib/core/help.ts
@@ -1,9 +1,10 @@
-import Pack from '../../package.json'
+import { resolvePkg } from '@utils/pkg'
 const program = require('commander')
 
-export function helpOptions(){
-  program.name(Pack.name)
-  program.version(Pack.version)
+export async function  helpOptions(){
+  const pkg = await resolvePkg(process.cwd())
+  program.name(pkg.name)
+  program.version(pkg.version)
   // program.option('-d, --dest <dest>','destination folder, exp: -d /src/pages')
   // program.option('-f, --framework <framework>','project framework, exp: vue')
   program.usage(`<command> [option]`)
